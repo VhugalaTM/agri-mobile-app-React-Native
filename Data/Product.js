69e109ed6105cb8f08be9b384665ad15ae5b9b34
VhugalaTM <154896814+VhugalaTM@users.noreply.github.com>
2026-01-10 01:50:56 +0200

    Add files via upload

---
+++ Data/Product.js
@@ -0,0 +1,148 @@
+import image1 from './images/1.jpg';
+import image2 from './images/2.jpg';
+import image3 from './images/3.jpg';
+import image4 from './images/4.jpg';
+import image5 from './images/5.jpg';
+import image6 from './images/6.jpg';
+import image7 from './images/7.jpg';
+import image8 from './images/8.jpg';
+import image9 from './images/9.jpg';
+import image10 from './images/10.jpg';
+import image11 from './images/11.jpg';
+import image12 from './images/12.jpg';
+import image13 from './images/13.jpg';
+
+
+export const PRODUCTS = [
+    {
+        id: 1,
+        productName: 'Paw Paw',
+        category: 'fruit',
+        price: 15.00,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates distinctio quod nihil perspiciatis accusamus ipsa dignissimos illo at, libero maxime laboriosam quam id! Amet consectetur quod necessitatibus perferendis provident perspiciatis. ',
+        productImage: image1,
+        rating: 'xxxxxxxxxxx',
+        stockAvailability: true
+    },
+    {
+        id: 2,
+        productName: 'Watermelon',
+        category: 'fruit',
+        price: 20.00,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates distinctio quod nihil perspiciatis accusamus ipsa dignissimos illo at, libero maxime laboriosam quam id! Amet consectetur quod necessitatibus perferendis provident perspiciatis.',
+        productImage: image2,
+        rating: 'xxxxxxxx',
+        stockAvailability: true
+    },
+    {
+        id: 3,
+        productName: 'Avocado',
+        category: 'vegetable',
+        price: 17.00,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates distinctio quod nihil perspiciatis accusamus ipsa dignissimos illo at, libero maxime laboriosam quam id! Amet consectetur quod necessitatibus perferendis provident perspiciatis.',
+        productImage: image3,
+        rating: 'xxxxxxxxxxx',
+        stockAvailability: true
+    },
+    {
+        id: 4,
+        productName: 'Bundle Vegies',
+        category: 'vegetable',
+        price: 100.00,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates distinctio quod nihil perspiciatis accusamus ipsa dignissimos illo at, libero maxime laboriosam quam id! Amet consectetur quod necessitatibus perferendis provident perspiciatis.',
+        productImage: image4,
+        rating: 'xxxxxxxxxxx',
+        stockAvailability: true
+    },
+    {
+        id: 5,
+        productName: 'Tomato',
+        category: 'vegetable',
+        price: 5.00,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates distinctio quod nihil perspiciatis accusamus ipsa dignissimos illo at, libero maxime laboriosam quam id! Amet consectetur quod necessitatibus perferendis provident perspiciatis.',
+        productImage: image5,
+        rating: 'xxxxxxxxxxx',
+        stockAvailability: true
+    },
+    {
+        id: 6,
+        productName: 'Mango',
+        category: 'fruit',
+        price: 6.00,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates distinctio quod nihil perspiciatis accusamus ipsa dignissimos illo at, libero maxime laboriosam quam id! Amet consectetur quod necessitatibus perferendis provident perspiciatis.',
+        productImage: image6,
+        rating: 'xxxxxxxxxxx',
+        stockAvailability: true
+    },
+    {
+        id: 7,
+        productName: 'Bundle fruits',
+        category: 'fruit',
+        price: 80.00,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates distinctio quod nihil perspiciatis accusamus ipsa dignissimos illo at, libero maxime laboriosam quam id! Amet consectetur quod necessitatibus perferendis provident perspiciatis.',
+        productImage: image7,
+        rating: 'xxxxxxxxxxx',
+        stockAvailability: true
+    },
+    {
+        id: 8,
+        productName: 'Peach',
+        category: 'fruit',
+        price: 8.00,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates distinctio quod nihil perspiciatis accusamus ipsa dignissimos illo at, libero maxime laboriosam quam id! Amet consectetur quod necessitatibus perferendis provident perspiciatis.',
+        productImage: image8,
+        rating: 'xxxxxxxxxxx',
+        stockAvailability: true
+    },
+    {
+        id: 9,
+        productName: 'Pineapple',
+        category: 'fruit',
+        price: 10.00,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates distinctio quod nihil perspiciatis accusamus ipsa dignissimos illo at, libero maxime laboriosam quam id! Amet consectetur quod necessitatibus perferendis provident perspiciatis.',
+        productImage: image9,
+        rating: 'xxxxxxxxxxx',
+        stockAvailability: true
+    },
+    {
+        id: 10,
+        productName: 'Grape',
+        category: 'fruit',
+        price: 17.00,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates distinctio quod nihil perspiciatis accusamus ipsa dignissimos illo at, libero maxime laboriosam quam id! Amet consectetur quod necessitatibus perferendis provident perspiciatis.',
+        productImage: image10,
+        rating: 'xxxxxxxxxxx',
+        stockAvailability: true
+    },
+    {
+        id: 11,
+        productName: 'Carrots',
+        category: 'fruit',
+        price: 25.00,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates distinctio quod nihil perspiciatis accusamus ipsa dignissimos illo at, libero maxime laboriosam quam id! Amet consectetur quod necessitatibus perferendis provident perspiciatis.',
+        productImage: image11,
+        rating: 'xxxxxxxxxxx',
+        stockAvailability: true
+    },
+    {
+        id: 12,
+        productName: 'Pier',
+        category: 'fruit',
+        price: 7.00,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates distinctio quod nihil perspiciatis accusamus ipsa dignissimos illo at, libero maxime laboriosam quam id! Amet consectetur quod necessitatibus perferendis provident perspiciatis.',
+        productImage: image12,
+        rating: 'xxxxxxxxxxx',
+        stockAvailability: true
+    },
+    {
+        id: 13,
+        productName: 'Strawberry',
+        category: 'fruit',
+        price: 16.00,
+        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates distinctio quod nihil perspiciatis accusamus ipsa dignissimos illo at, libero maxime laboriosam quam id! Amet consectetur quod necessitatibus perferendis provident perspiciatis.',
+        productImage: image13,
+        rating: 'xxxxxxxxxxx',
+        stockAvailability: true
+    },
+    
+]
